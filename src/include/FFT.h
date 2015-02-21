bfe2e57d44fe2d22609d4f527f09ec79733173a8
Damian Pfammatte <pfammatterdamian@gmail.com>
2015-02-21 13:02:29 +0000

    Licensing

--- src/include/FFT.h
+++ src/include/FFT.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2015 by Damian Pfammatter <pfammatterdamian@gmail.com>
+ *
+ * This file is part of RTL-spec.
+ *
+ * RTL-Spec is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * RTL-Spec is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RTL-Spec.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef FFT_H /* Fast Fourier Transform */
 #define FFT_H
 
