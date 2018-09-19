5f0ff5ab2b6222649689e25121679f49f81c04c9
Roberto Calvo <rocapal@gmail.com>
2018-09-19 09:48:39 +0200

    allow IQ sampling without sleep #2

--- drivers/rtlsdr/rtlsdrDriver.cpp
+++ drivers/rtlsdr/rtlsdrDriver.cpp
@@ -247,36 +247,37 @@ bool rtlsdrDriver::isRunning () {
 
 static void *aux_thread (void *arg) {
 
-	struct timespec ref_time;
+    struct timespec ref_time;
 
     ref_time.tv_sec = ElectrosenseContext::getInstance()->getStartTimeSampling();
     ref_time.tv_nsec = 0;
 
-    struct timespec time_diff;
+    callback_package_t *cp_p = (callback_package_t *) arg;
+    callback_package_t &cp = *cp_p;
 
+    if (ElectrosenseContext::getInstance()->getStartTimeSampling() != 0) {
 
-    callback_package_t * cp_p=(callback_package_t *)arg;
-    callback_package_t & cp=*cp_p;
+        struct timespec time_diff;
 
-    // Sleep until the absolute time is fulfilled
-    struct timespec currentTime;
-    clock_gettime(CLOCK_REALTIME, &currentTime);
+        // Sleep until the absolute time is fulfilled
+        struct timespec currentTime;
+        clock_gettime(CLOCK_REALTIME, &currentTime);
 
-    timespec_diff(&currentTime,&ref_time,&time_diff);
+        timespec_diff(&currentTime, &ref_time, &time_diff);
 
-    if (time_diff.tv_sec < 0 || time_diff.tv_nsec < 0)
-    {
-        std::cerr << "[ERROR] Starting Time is in the past! " << std::endl;
-        pthread_exit(NULL);
+        if (time_diff.tv_sec < 0 || time_diff.tv_nsec < 0) {
+            std::cerr << "[ERROR] Starting Time is in the past! " << std::endl;
+            pthread_exit(NULL);
+        }
+
+        std::cout << "--- IQ Sync Sampling ---" << std::endl;
+        std::cout << "CurrentTime: " << currentTime.tv_sec << "." << currentTime.tv_nsec << std::endl;
+        std::cout << "StartTime Sampling: " << ref_time.tv_sec << "." << ref_time.tv_nsec << std::endl;
+        std::cout << "Sleeping ... " << std::endl;
+        std::cout << "--- End IQ Sync Sampling ---" << std::endl << std::endl;
+
+        clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &ref_time, NULL);
     }
-
-    std::cout << "--- IQ Sync Sampling ---" << std::endl;
-    std::cout << "CurrentTime: " << currentTime.tv_sec << "." << currentTime.tv_nsec << std::endl;
-    std::cout << "StartTime Sampling: " << ref_time.tv_sec << "." << ref_time.tv_nsec << std::endl;
-    std::cout << "Sleeping ... " << std::endl;
-    std::cout << "--- End IQ Sync Sampling ---" << std::endl << std::endl;
-
-    clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &ref_time, NULL);
 
     rtlsdr_read_async(cp.dev, capbuf_rtlsdr_callback,arg,0,0);
     pthread_exit(NULL);
